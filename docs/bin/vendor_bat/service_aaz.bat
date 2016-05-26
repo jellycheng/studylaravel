fa79c24460453f317ac7c4c3c0efa10ecbee89bc
jelly <42282367@qq.com>
2016-05-26 16:14:40 +0800

    jelly

--- docs/bin/vendor_bat/service_aaz.bat
+++ docs/bin/vendor_bat/service_aaz.bat
@@ -1,5 +1,5 @@
 
-cd /d D:\pingan\laravel_v5\cloud\ananzu-service
+cd /d D:\pingan\laravel_v5\cloud\xxx-service
 rd /S /Q vendor
 mkdir vendor
 composer update -vvv
--- docs/bin/vendor_bat/touchweb_aaz.bat
+++ docs/bin/vendor_bat/touchweb_aaz.bat
@@ -1,5 +1,5 @@
 
-cd /d D:\pingan\laravel_v5\cloud\ananzu-touchweb
+cd /d D:\pingan\laravel_v5\cloud\xxx-touchweb
 rd /S /Q vendor
 mkdir vendor
 composer update -vvv
